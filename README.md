a340a09661f692fac356f7cba0bcd091ab419d15
jxxchallenger <jxxchallenger@foxmail.com>
2020-01-11 23:17:56 +0800

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# log4j2 samples
+阅读[Apache Log4j 2](http://logging.apache.org/log4j/2.x/)官方在线文档
---
+++ configuration/src/main/java/io/jxxchallenger/configuration/Bar.java
@@ -0,0 +1,15 @@
+package io.jxxchallenger.configuration;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Bar {
+
+    static final Logger logger = LogManager.getLogger(Bar.class.getName());
+    
+    public boolean doIt() {
+      logger.traceEntry();
+      logger.error("Did it again!");
+      return logger.traceExit(false);
+    }
+}
